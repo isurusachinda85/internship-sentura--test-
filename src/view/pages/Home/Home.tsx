737ba1de8edaaf87aaed0b406e011cfa40d0167a
isurusachinda85 <isurusachintha8@gmail.com>
2024-02-03 10:12:32 +0530

    update

---
+++ src/view/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+import {UserForm} from "../../common/userForm/UserForm";
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <UserForm/>
+            </div>
+        );
+    }
+}
